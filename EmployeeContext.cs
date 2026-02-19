dd940fbfed1a284a3e12f66e44fb88908a7ad1d5
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2026-02-18 20:28:11 -0500

    Add automatic period updating

--- EmployeeContext.cs
+++ EmployeeContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using NpgsqlTypes;
 
 namespace NpgTemporalTest;
 
@@ -27,5 +29,54 @@ public class EmployeeContext : DbContext
         });
 
         //modelBuilder.Entity<Employee>().HasTemporalKey("EmployeeId", "ValidPeriod");
+    }
+
+    // TODO: Use a Savechanges interceptor instead.
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ChangeTracker.DetectChanges();
+
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            var prop = entry.Metadata.FindPrimaryKey().Properties[^1];
+            
+            // Hack! Use an annotation instead.
+            if (prop.ClrType != typeof(NpgsqlRange<DateTime>))
+                continue;
+
+            NpgsqlRange<DateTime> period = (NpgsqlRange<DateTime>)entry.CurrentValues[prop];
+            DateTime now = DateTime.UtcNow;
+
+            switch (entry.State)
+            {
+                case EntityState.Modified:
+                    // Update the record to be valid from here to infinity.
+                    entry.CurrentValues[prop] = new NpgsqlRange<DateTime>(now, true, DateTime.MaxValue, false);
+                    
+                    // Construct an old version of the record to replace the one being modified.
+                    object oldEntry = entry.Entity.GetType().GetConstructor(System.Type.EmptyTypes).Invoke(null);
+
+                    // TODO: Get rid of this step and rewrite this whole function so that we're inserting instead of modifying.
+                    var oldValues = await entry.GetDatabaseValuesAsync();
+
+                    // Set the old version to retain its original starting period but end its validity at the current time.
+                    oldValues[prop] = new NpgsqlRange<DateTime>(period.LowerBound, true, now, false);
+
+                    // Copy the pre-modified values over.
+                    foreach (var property in oldValues.Properties)
+                    {
+                        property.PropertyInfo.SetValue(oldEntry, oldValues[property]);
+                    }
+
+                    Add(oldEntry);
+                    break;
+
+                case EntityState.Deleted:
+                    entry.CurrentValues[prop] = new NpgsqlRange<DateTime>(period.LowerBound, true, now, false);
+                    break;
+            }
+        }
+
+        return await base.SaveChangesAsync();
     }
 }
